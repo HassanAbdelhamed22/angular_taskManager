900d075919103b1302e486a020c456502606a47d
HassanAbdelhamed22 <104987743+HassanAbdelhamed22@users.noreply.github.com>
2026-04-15 14:29:33 +0200

    feat: add carousel component with styles and functionality
    - Created carousel component with HTML, CSS, and TypeScript files.
    - Implemented image sliding functionality with next, previous, and indicator navigation.
    - Added home component to integrate the carousel.
    - Included basic styles for carousel layout and buttons.

---
+++ src/app/components/carousel/carousel.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  imports: [],
+  templateUrl: './carousel.html',
+  styleUrl: './carousel.css',
+})
+export class Carousel {
+  images = ['1.jpg', '2.jpg', '3.jpg'];
+  currentIndex = 0;
+
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prev() {
+    this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+
+  goTo(index: number) {
+    this.currentIndex = index;
+  }
+}
